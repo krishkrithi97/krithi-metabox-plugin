e1a2ddf399bb60e413762d755220fb425888a275
krishkrithi97 <79185460+krishkrithi97@users.noreply.github.com>
2021-03-18 16:09:07 +0530

    Update poemhd-plugin.php

--- poemhd-plugin.php
+++ poemhd-plugin.php
@@ -6,7 +6,7 @@
  * Plugin Name:       Poemhd Plugin
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Handle the basics with this plugin.
- * Version:           1.10.3
+ * Version:           1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Krithi Krishna
